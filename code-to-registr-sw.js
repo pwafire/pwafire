6c33d368caccb0648eef96e2ae0b6e2aa56a294c
Maye Edwin <mayedwine1@gmail.com>
2018-02-14 16:59:43 +0300

    Create code-to-registr-sw.js

---
+++ code-to-registr-sw.js
@@ -0,0 +1,16 @@
+<!-- register service worker -->
+
+	<script>
+	
+	    if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js')
+    .then(function() { console.log("Service Worker Registered"); });
+  }
+  
+  );
+}
+  
+ </script>
+ 
+		<!-- end of service worker -->
