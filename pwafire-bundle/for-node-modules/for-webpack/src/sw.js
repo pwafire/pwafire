7a67e21f73599cce7ece134b7a949198c1c7e108
Maye Edwin <mayedwine1@gmail.com>
2018-05-29 11:26:50 +0300

    clean
    clean

--- pwafire-bundle/for-node-modules/for-webpack/src/sw.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
-if (workbox) {
-console.log(`Yay! PWA Fire with Workbox is loaded 🎉`);
-workbox.precaching.precacheAndRoute([]);
- } else {
- console.log(`Oops! PWA Fire with Workbox didn't load 😬`);
- }
