0cf203fc1d424062686b239ab2b78389d04a6517
Maye Edwin <mayedwine1@gmail.com>
2018-08-31 08:47:59 +0300

    update-service-worker-to-latest-pwafireapp
    update-service-worker-to-latest-pwafireapp

--- pwafire-bundle/for-npm/src/service-worker.js
+++ pwafire-bundle/for-npm/src/service-worker.js
@@ -1,12 +1,60 @@
 // authored by Maye Edwin : https://twitter.com/MayeEdwin1
 // Add offline properties, push notification, web share, web payments, etc
 // pwafire 4.0.0
-// Credits : 2018 Google Inc.
+
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js')
   
 if (workbox) {
-    console.log(`Yay! Workbox is loaded ! Cheers to PWA Fire🎉`);
+    console.log(`Yay! Workbox is loaded ! Cheers to PWA Fire 🐹`);
     workbox.precaching.precacheAndRoute([]);
+    
+   /*  cache images in the e.g others folder; edit to other folders you got 
+   and config in the sw-config.js file
+    */
+   workbox.routing.registerRoute(
+    /(.*)others(.*)\.(?:png|gif|jpg)/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        })
+      ]
+    })
+  );
+    /* Make your JS and CSS ⚡ fast by returning the assets from the cache, 
+  while making sure they are updated in the background for the next use.
+  */
+  workbox.routing.registerRoute(
+    // cache js, css, scc files 
+    /.*\.(?:css|js|scss|)/,
+    // use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+      // use a custom cache name
+      cacheName: 'assets',
+    })
+  );
+
+   // cache google fonts
+  workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'google-fonts',
+      plugins: [
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200],
+        }),
+      ],
+    }),
+  ); 
+
+  // add offline analytics 
+  workbox.googleAnalytics.initialize(); 
+
+workbox.skipWaiting();
+workbox.clientsClaim();
+    
 } else {
-    console.log(`Oops! Workbox didn't load 😬`);
+    console.log(`Oops! Workbox didn't load 👺`);
 }
