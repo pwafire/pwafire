b10bb7bad6eaec20db9d73cebacabd79ab356a53
Maye Edwin <mayedwine1@gmail.com>
2018-12-13 23:07:52 +0300

    add js

---
+++ bundle/badging/badge.js
@@ -0,0 +1,15 @@
+
+`javascript
+
+// In a web page
+const unreadCount = 24;
+window.Badge.set(unreadCount);
+
+// In a service worker
+self.addEventListener('sync', () => {
+  self.Badge.set(getUnreadCount());
+});
+
+`
+
+
