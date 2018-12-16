44125074c9f131257e138b63e935796ff493881a
Maye Edwin <mayedwine1@gmail.com>
2018-12-16 13:54:11 +0300

    Create render.js

---
+++ bundle/render/render.js
@@ -0,0 +1,11 @@
+/* 
+Fast page loads result in higher user engagement, more pageviews, 
+and improved conversion.
+
+You can improve your page load speed by inlining links and scripts that
+are required for first paint, and deferring those that aren't. */
+
+window.onload = function()
+{
+    document.getElementById('render').removeAttribute('disabled');
+};
