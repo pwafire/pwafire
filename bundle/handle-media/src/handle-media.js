7136e53c146a975482488e54a4b49e8bdaf7aaae
Maye Edwin <mayedwine1@gmail.com>
2019-06-04 15:09:01 +0300

    Create handle-media.js

---
+++ bundle/handle-media/src/handle-media.js
@@ -0,0 +1,12 @@
+/* check if the Network Information API is supported / available */
+if (navigator.connection.effectiveType === '4G') {
+// load a video on 4G network
+  const video = document.getElementById('appVideo');
+  const videoSource = video.getAttribute('data-src');
+  video.setAttribute('src', videoSource);
+} else {
+// load an image on 3G and less
+  const image = document.getElementById('coverImage');
+  const imageSource = image.getAttribute('data-src');
+  image.setAttribute('src', imageSource);
+}
