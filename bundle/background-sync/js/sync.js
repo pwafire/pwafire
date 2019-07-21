887a7d1e7a7331720a3fe2582ad7b56f159a4426
Maye Edwin <mayedwine1@gmail.com>
2019-07-21 12:18:12 +0300

    arrow function syntax

--- bundle/background-sync/js/sync.js
+++ bundle/background-sync/js/sync.js
@@ -1,29 +1,26 @@
 
-// Author : Maye Edwin https://pwafire.org
-
-(function (window) {
+// learn more at : https://pwafire.org
+((window) => {
     'use strict';
     // Add background sync on page load
-    document.addEventListener("DOMContentLoaded", function() {
+    document.addEventListener("DOMContentLoaded", () => {
       window.registerBGSync();
     });
     // Exposing `registerSync()` globally for only development purpose
-    window.registerBGSync = function() {
+    window.registerBGSync = () => {
       //If `serviceWorker` is registered and ready
       navigator.serviceWorker.ready
-        .then(function (registration) {
+        .then((registration) => {
           //Registering `background sync` event
           return registration.sync.register('Comments') 
            // `Comments` is sync tag name ; could be anything 😉
-            .then(function (rs) {
+            .then((rs) => {
               console.info('Background sync registered!');
-            }, function () {
+            }, () => {
               console.error('Background sync registered failed.');
             });
   
         });
-    };
-  })
-  (window);
-
+    }
+  })(window);
 
