ce5732b44bbcd7f584fbd2ac2c421681ecdd7ab1
Maye Edwin <mayedwine1@gmail.com>
2019-07-21 12:18:53 +0300

    Update sync.js

--- bundle/background-sync/js/sync.js
+++ bundle/background-sync/js/sync.js
@@ -8,10 +8,10 @@
     });
     // Exposing `registerSync()` globally for only development purpose
     window.registerBGSync = () => {
-      //If `serviceWorker` is registered and ready
+      // If `serviceWorker` is registered and ready
       navigator.serviceWorker.ready
         .then((registration) => {
-          //Registering `background sync` event
+          // Registering `background sync` event
           return registration.sync.register('Comments') 
            // `Comments` is sync tag name ; could be anything 😉
             .then((rs) => {
