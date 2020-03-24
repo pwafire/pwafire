04c7a892ca7fee13c6a314977ab4362ebbe0e1a8
Maye Edwin <mayedwine1@gmail.com>
2020-03-25 00:56:47 +0300

    Show if PWA is installed
    Show if PWA is installed

--- bundle/install/button/Install.js
+++ bundle/install/button/Install.js
@@ -1,32 +1,32 @@
-
 /* add custom install banner @mayeedwin @pwafire 2020 */
-const buttonInstall = document.getElementById('buttonInstall');
+const installButton = document.getElementById('install');
 
 window.addEventListener('beforeinstallprompt', (event) => {
-  console.log('👍', 'beforeinstallprompt', event);
+  console.log('👍', 'Before Install Prompt');
   // Stash the event so it can be triggered later.
   window.deferredPrompt = event;
   // Remove the 'hidden' class from the install button container
-  buttonInstall.removeAttribute('disabled');
+  // installButton.removeAttribute('disabled');
 });
 
-buttonInstall.addEventListener('click', () => {
-  console.log('👍', 'buttonInstall-clicked');
+installButton.addEventListener('click', () => {
+  console.log('👍', 'Install Button Clicked');
   const promptEvent = window.deferredPrompt;
   if (!promptEvent) {
-    // The deferred prompt isn't available.
-    return;
+    // The deferred prompt isn't available, so pwa exists
+    console.log(`App Exists`);
+    installButton.innerHTML = `<i class="fas fa-tablet-alt"></i>&nbsp;&nbsp;Already Installed`
+    return installed;
   }
   // Show the install prompt.
   promptEvent.prompt();
   // Log the result
   promptEvent.userChoice.then((result) => {
-    console.log('👍', 'userChoice', result);
+    console.log('👍', 'User Choice', result);
     // Reset the deferred prompt variable, since 
     // prompt() can only be called once.
     window.deferredPrompt = null;
     // Hide the install button.
-    buttonInstall.setAttribute('disabled', true);
   });
 });
 
