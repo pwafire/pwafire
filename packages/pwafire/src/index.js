dce15b3670551ed1ca1f1c07ac3ad5187cddb7ae
Maye Edwin <mayedwine1@gmail.com>
2020-08-07 01:49:30 +0300

    Updated  => added return objects

--- packages/pwafire/src/index.js
+++ packages/pwafire/src/index.js
@@ -2,48 +2,71 @@
 class PWA {
   // Copy text...
   async copyText(text) {
-      try {
-        await navigator.clipboard.writeText(text);
-      } catch (err) {
-        console.error("Failed to copy to clipboard", err);
-      }
+    try {
+      await navigator.clipboard.writeText(text);
+      // Copied...
+      return { code: "copied", message: `Copied text to clipboard` };
+    } catch (error) {
+      // Error...
+      return {
+        error,
+        code: "failed",
+        message: `Failed to copy text to clipboard`,
+      };
+    }
   }
   // Web Share...
   Share(data) {
-      // Check if web share is supported
-      if (navigator.share) {
-        navigator
-          .share(data)
-          .then(() => console.log(`Shared`))
-          .catch((error) => console.log(`Error sharing`, error));
-      } else {
-        console.log(`Web share not supported!`);
-      };
+    // Check support...
+    if (navigator.share) {
+      navigator
+        .share(data)
+        .then(() => {
+          // Shared...
+          return { code: "shared", message: `Shared data` };
+        })
+        .catch(error => {
+          // Error..
+          return { error, code: "failed", message: `Failed to share data` };
+        });
+    } else {
+      // No support...
+      return { code: "no-support", message: `Not supported` };
+    }
   }
+
   // Contacts Picker...
   async Contacts(props, options) {
-      try {
-        const contacts = await navigator.contacts.select(props, options);
-        // Return contacts...
-        return contacts;
-      } catch (error) {
-        // Handle any errors here...
-        alert(error);
-      }
+    try {
+      const contacts = await navigator.contacts.select(props, options);
+      // Return contacts...
+      return contacts;
+    } catch (error) {
+      // Error...
+      return { error, code: "failed", message: `Not supported` };
+    }
   }
+
   // Connectivity...
-  Connectivity(online, offline) {
-    // Once the DOM is loaded, check for connectivity...
-    document.addEventListener("DOMContentLoaded", () => {
-      // Offline Event...
-      if (!navigator.onLine) {
-        offline();
-      }
-      // Online Event...
-      window.addEventListener("online", () => {
-        online();
+  async Connectivity(online, offline) {
+    // Check support...
+    try {
+      document.addEventListener("DOMContentLoaded", () => {
+        // Offline Event...
+        if (!navigator.onLine) {
+          offline();
+          return { code: "offline", message: `Offline` };
+        }
+        // Online Event...
+        window.addEventListener("online", () => {
+          online();
+          return { code: "online", message: `Online` };
+        });
       });
-    });
+    } catch (error) {
+      // Error...
+      return { error, code: "failed", message: `Not supported` };
+    }
   }
   // Copy image
   async copyImage(imgURL) {
@@ -58,98 +81,108 @@ class PWA {
           })
         ),
       ]);
-      console.log("Image copied.");
-      return `Image copied.`;
-    } catch (e) {
-      console.error(e, e.message);
+      // Error...
+      return { code: "copied", message: `Copied image to clipboard` };
+    } catch (error) {
+      // Error...
+      return { error, code: "failed", message: `Failed` };
     }
   }
   // Badge...
-  Badge(unreadCount) {
-    return {
-      get Set() {
-        navigator.setAppBadge(unreadCount).catch((error) => {
-          // Do something with the error.
-          console.log(error);
-        });
-        return `Set bage`;
-      },
-      get Clear() {
-        // Clear the badge
-        navigator.clearAppBadge().catch((error) => {
-          // Do something with the error.
-          console.log(error);
-        });
-        return `Clear badge`;
-      },
-    };
+  async Badge(unreadCount) {
+    try {
+      return {
+        get Set() {
+          navigator.setAppBadge(unreadCount).catch(error => {
+            // Do something with the error.
+            return { error, code: "failed", message: `Failed` };
+          });
+          return { code: "set", message: `Set badge` };
+        },
+        get Clear() {
+          // Clear the badge
+          navigator.clearAppBadge().catch(error => {
+            // Do something with the error.
+            return { error, code: "failed", message: `Failed` };
+          });
+          return { code: "clear", message: `Clear badge` };
+        },
+      };
+    } catch (error) {
+      return { error, code: "failed", message: `Not supported` };
+    }
   }
   // Payment...
   Payment(paydata, validatePayment) {
-      const paymentRequest = new PaymentRequest(
-        paydata.paymentMethods,
-        paydata.paymentDetails,
-        paydata.options
-      );
-      // Initiate user interface...
-      paymentRequest
-        .show()
-        .then((paymentResponse) => {
-          // Validate with backend...
-          validatePayment(paymentResponse);
-        })
-        .catch((err) => {
-          // API error or user cancelled the payment
-          console.log("Error:", err);
-        });
+    const paymentRequest = new PaymentRequest(
+      paydata.paymentMethods,
+      paydata.paymentDetails,
+      paydata.options
+    );
+    // Initiate user interface...
+    paymentRequest
+      .show()
+      .then(paymentResponse => {
+        // Validate with backend...
+        validatePayment(paymentResponse);
+        return { code: "succesful", message: `Successful`, paymentResponse };
+      })
+      .catch(error => {
+        // API error or user cancelled the payment
+        return { error, code: "failed", message: `Failed` };
+      });
   }
   // Fullscreen...
-  Fullscreen() {
+  async Fullscreen() {
+    try {
       if (document.fullscreenEnabled) {
         document.documentElement.requestFullscreen();
       }
+    } catch (error) {
+      // Error...
+      return { error, code: "failed", message: `Failed` };
+    }
   }
   // Notification...
   Notification(data) {
     const { title, options } = data;
     if ("Notification" in window) {
       Notification.requestPermission()
-        .then((permission) => {
+        .then(permission => {
           if (permission === "granted") {
-            navigator.serviceWorker.ready.then((registration) => {
+            navigator.serviceWorker.ready.then(registration => {
               registration.showNotification(title, options);
+              // Sent...
+              return { code: "sent", message: `Notification sent` };
             });
           }
         })
-        .catch((error) => {
-          console.log(error);
+        .catch(error => {
+          return { error, code: "failed", message: `Failed` };
         });
     } else {
-      return `Notifications not supported.`;
+      return { code: "no-support", message: `Not supported` };
     }
   }
   // Install...
   Install() {
-    window.addEventListener("beforeinstallprompt", (event) => {
+    window.addEventListener("beforeinstallprompt", event => {
       // Stash the event so it can be triggered later...
       window.deferredPrompt = event;
     });
-      console.log(`"👍", Install Button Clicked`);
-      const promptEvent = window.deferredPrompt;
-      if (!promptEvent) {
-        // The deferred prompt isn't available, so pwa exists...
-        console.log(`App Exists`);
-        return null;
-      }
-      // Show the install prompt...
-      promptEvent.prompt();
-      // Log the result...
-      promptEvent.userChoice.then((result) => {
-        // Reset the deferred prompt variable, since rompt() can only be called once...
-        window.deferredPrompt = null;
-      });
-    window.addEventListener("appinstalled", (event) => {
-      console.log(`"👍", App Installed`);
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      return { code: "exists", message: `App exists` };
+    }
+    // Show the install prompt...
+    promptEvent.prompt();
+    // Log the result...
+    promptEvent.userChoice.then(result => {
+      // Reset the deferred prompt variable, since rompt() can only be called once...
+      window.deferredPrompt = null;
+    });
+    window.addEventListener("appinstalled", event => {
+      return { code: "installed", message: `App installed` };
     });
   }
   // Visibility...
@@ -159,10 +192,12 @@ class PWA {
       if (state === `visible`) {
         // Call back function...
         isVisible();
+        return { code: "visible", message: `Content is visible` };
       }
     } else {
       // Alternative...
       notAvailable();
+      return { code: "no-support", message: `Not available` };
     }
   }
 }
