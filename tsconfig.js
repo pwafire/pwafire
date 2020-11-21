ef0c6762ddb36e8fd970c84a457cdb1bd1bd9ff0
Maye Edwin <mayedwine1@gmail.com>
2020-11-21 11:07:51 +0300

    Create tsconfig.js

---
+++ tsconfig.js
@@ -0,0 +1,12 @@
+{
+  "compilerOptions": {
+    "target": "ES2015",
+    "module": "commonjs",
+    "declaration": true,
+    "outDir": "./packages/pwafire/lib",
+    "strict": true,
+    "allowJs": true
+  },
+  "include": ["src"],
+  "exclude": ["node_modules", "**/tests/*"]
+}
