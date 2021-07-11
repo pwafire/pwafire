e4776416f203f6c57b6ba5c0702d50f77aef5b1a
Maye Edwin <em@sky.garden>
2021-07-11 12:37:18 +0300

    remove duplicate type

--- packages/pwafire/src/types.d.ts
+++ packages/pwafire/src/types.d.ts
@@ -1,8 +1,6 @@
 declare class ClipboardItem {
   constructor(data: { [mimeType: string]: Blob });
 }
-
-declare let wakeLock: any;
 interface Window {
   addEventListener(arg0: string, arg1: () => { type: string; message: string }): any;
   deferredPrompt?: any;
