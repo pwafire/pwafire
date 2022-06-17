a29542e1d75b8ef46806b5bafaf1913f34b77363
Maye Edwin <mayedwine1@gmail.com>
2022-06-17 12:07:31 +0300

    Feature : Add more display mode types

--- packages/pwafire/src/pwa/index.ts
+++ packages/pwafire/src/pwa/index.ts
@@ -500,10 +500,16 @@ class PWA {
    * Get the display mode of your progressive web app.
    * @method displayMode
    */
-  async displayMode(callback: (mode: "standalone" | "tab") => void) {
+  async displayMode(callback: (mode: "standalone" | "minimal-ui" | "fullscreen" | "broswer-tab") => void) {
     try {
       window.addEventListener("DOMContentLoaded", () => {
-        const displayMode = window.matchMedia("(display-mode: standalone)").matches ? "standalone" : "tab";
+        const displayMode = window.matchMedia("(display-mode: standalone)").matches
+          ? "standalone"
+          : window.matchMedia("(display-mode: minimal-ui)").matches
+          ? "minimal-ui"
+          : window.matchMedia("(display-mode: fullscreen)").matches
+          ? "fullscreen"
+          : "broswer-tab";
         callback(displayMode);
       });
     } catch (error) {
